2aa119dcce09ea327bfbc5ef0253b1739b42df4f
Trong Trinh Dinh <dinhtrongtrinh04@gmail.com>
2026-03-24 09:46:12 +0100

    hotovy hw1 s chatem

--- template/hw1.rkt
+++ template/hw1.rkt
@@ -30,37 +30,43 @@
 ;                   START YOUR IMPLEMENTATION BELOW
 ; -----------------------------------------------------------------------------
 
-;;; Takes an image and converts it into a list of lists of grayscale values.
 (define (list-split lst widht [answer '()])
   (if (empty? lst)
     (reverse answer)
-    
-    (list-split (drop lst widht) widht (cons (take lst widht) answer)))
-  )
+    (list-split (drop lst widht) widht (cons (take lst widht) answer))))
 
+;;; Takes an image and converts it into a list of lists of grayscale values.
 (define (img->mat image)
   (let ([widht (image-width image)]
         [color (image->color-list image)])
-        (list-split (map color->gray color) widht)))
+    (list-split (map color->gray color) widht)))
+
 ;;; First, takes a specification consisting of block-width, block-height and
 ;;; a character-set; then returns a new function which can convert any image
 ;;; into a 2D ascii-art string.
 (define (ascii-art block-width block-height charset)
   (define (convert image)
-  (let* ([matice (img->mat image)]
-         [len (string-length charset)]
-         ;; 1. KROK: Převedeme čísla na znaky (2D seznam)
-         [matice-znaku 
-          (map (lambda (radek)
-                 (map (lambda (pixel) 
-                        (string-ref charset (index-formula len pixel)))
-                      radek))
-               matice)]
-         ;; 2. KROK: Každý řádek (seznam znaků) změníme na řetězec
-         [seznam-radku (map list->string matice-znaku)])
-    
-    ;; 3. KROK: Spojíme řádky do jednoho textu s novými řádky
-    (string-join seznam-radku "\n")))
+    (let* ([matice (img->mat image)]
+           [len (string-length charset)]
+           [rows (length matice)]
+           [cols (if (empty? matice) 0 (length (first matice)))]
+           [valid-rows (* block-height (quotient rows block-height))]
+           [valid-cols (* block-width (quotient cols block-width))]
+           [cropped-mat (take matice valid-rows)]
+           [cropped-mat (map (lambda (row) (take row valid-cols)) cropped-mat)]
+           [bands (list-split cropped-mat block-height)]
+           [process-band 
+            (lambda (band)
+              (let* ([chunked-rows (map (lambda (row) (list-split row block-width)) band)]
+                     [blocks (apply map list chunked-rows)])
+                (map (lambda (block)
+                       (let* ([pixels (apply append block)]
+                              [avg (/ (apply + pixels) (length pixels))])
+                         (string-ref charset (index-formula len avg))))
+                     blocks)))]
+           [matice-znaku (map process-band bands)]
+           [seznam-radku (map list->string matice-znaku)])
+      (string-join seznam-radku "\n")))
   convert)
 
 ; Hint: If You decide to modify the list-split function from lab2,
